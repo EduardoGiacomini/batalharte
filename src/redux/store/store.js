a0481694b46d55e9fcbfe2b676ab12de8adf9b40
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2018-11-15 09:15:13 -0300

    Removendo state do redux para visualização pública

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,11 +1,7 @@
 import { createStore } from 'redux';
 import reducers from '../reducers';
 
-// Configuration devtools
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
-    && window.__REDUX_DEVTOOLS_EXTENSION__();
-
 // Conect store with reducers
-const store = createStore(reducers, devTools);
+const store = createStore(reducers);
 
 export default store;
